c72a261166ad80ab8fb2fdbab61cb770b1928285
Ed Bosher <edbosher@gmail.com>
2014-04-15 23:37:47 +0800

    updating for bitcion 0.9

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "andrew@bedesign.be"
 license          "MIT"
 description      "Installs/Configures crypto-coin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.2"
+version          "0.9.1"
 
 depends          "apt"
 
